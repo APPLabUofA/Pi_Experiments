acf01cda1156636a64ba4a8a249b9ea4701e3998
kuziekj <kuziek@ualberta.ca>
2019-06-21 13:30:08 -0600

    Add files via upload

--- Visual_P3_LED/Task/Visual_P3_LED.py
+++ Visual_P3_LED/Task/Visual_P3_LED.py
@@ -1,18 +1,12 @@
-import time
-import os
-import sys
-from random import randint, shuffle
-import blinkt
-import datetime
-import numpy
-import pygame
+##import all the needed packages##
+import board
+import neopixel
 import RPi.GPIO as GPIO
+import time
+from random import randint, shuffle
+import numpy as np
+import os
 from pylsl import StreamInfo, StreamOutlet, local_clock
-
-partnum = sys.argv[1]
-device = sys.argv[2]
-filename = sys.argv[3]
-exp_loc = sys.argv[4]
 
 ###create our stream variables###
 info = StreamInfo('Markers', 'Markers', 1, 0, 'int32', 'myuidw43536')
@@ -20,217 +14,252 @@ info = StreamInfo('Markers', 'Markers', 1, 0, 'int32', 'myuidw43536')
 ###next make an outlet to record the streamed data###
 outlet = StreamOutlet(info)
 
-###initialise pygame###
-GPIO.setmode(GPIO.BCM)
-pygame.init()
-pygame.display.init()
-
-###setup pins for triggers###
-GPIO.setup([4,17,27,22,5,6,13,19],GPIO.OUT)
-
-###Setup our function to send triggers###
-###trig_type is either 's' or 'r'###
-###trig_num must be between 1-15###
-def pi2trig(trig_type,trig_num):
-    
-    if trig_type == 's':
-        pi_pins = [4,17,27,22]
-    elif trig_type == 'r':       
-        pi_pins = [5,6,13,19]
-    
-    bin_num = list(reversed(bin(trig_num)[2:]))
-    
-    while len(bin_num) < len(pi_pins):
-        bin_num.insert(len(bin_num)+1,str(0))
-    
-    trig_pins = []
-    
-    trig_pos = 0
-    
-    for i_trig in range(len(pi_pins)):
-        if bin_num[i_trig] == '1':
-            trig_pins.insert(trig_pos,pi_pins[i_trig])
-            trig_pos = trig_pos + 1
-    
-    return trig_pins
-
-###setup the display screen and fixation###
-pygame.mouse.set_visible(0)
-disp_info = pygame.display.Info()
-screen = pygame.display.set_mode((disp_info.current_w, disp_info.current_h),pygame.FULLSCREEN)
-x_center = disp_info.current_w/2
-y_center = disp_info.current_h/2
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255,255,255)
-
-###FOR DEBUG###
-#screen = pygame.display.set_mode((200,100),pygame.RESIZABLE)
-#x_center = 200/2
-#y_center = 100/2
-#####
-
-##### Define variables #####
+##setup some constant variables##
 partnum = "001"
+filename = 'visual_p3_gopro_visor'
 
-###define the number of trials, and tones per trial###
-trials = 10
-low_rate = 0.8
-high_rate = 0.2
-low_tone = numpy.zeros(int(trials*low_rate))
-high_tone = numpy.ones(int(trials*high_rate))
-low_tone_list = low_tone.tolist()
-high_tone_list = high_tone.tolist()
-tones = low_tone_list + high_tone_list
-shuffle(tones)
+##number of trials##
+trial_num = 10
 
-vid_time  = []
+##number of blocks
+block_num = 1
+
+##standard and target rate##
+standard_rate = 0.8
+target_rate = 0.2
+
+##several colours for the pixels##
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+blank = (0, 0, 0)
+brightness = 0.2
+
+##number of pixels we will be controlling##
+pin_num = 6
+
+##specify which pin we will be controlling the LEDs with##
+pin_out = board.D18
+
+##pins we will be using##
+muse_pin = 5
+resp_pin = 21
+
+##amount of time needed to reset triggers##
+trig_gap = 0.005
+
+###initialise GPIO pins###
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(muse_pin,GPIO.OUT)
+
+###set triggers to 0###
+GPIO.output(muse_pin,0)
+
+###setup pin for push button###
+GPIO.setup(resp_pin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+def resp_trig(trig): # maps response trigger to standard (3) or target (4)
+    if trig == 1:
+        resp_trig = 3
+    else:
+        resp_trig = 4
+
+def get_resp_led_off(pin, led_on_time,trig): # get response (if occured in first 1 second) + turn off the LEDs regardless
+    start_resp = time.time()
+
+    GPIO.wait_for_edge(pin,GPIO.RISING, timeout = int(led_on_time * 1000))
+
+    button_down = time.time() - start_resp # this is response time from the start of the 1 second response window
+
+    if button_down < led_on_time: ## right now this isn't making any sense to me
+        resp_trig(trig)
+        resp_time = button_down
+        if button_down <= 0.990:
+            time.sleep(led_on_time - (button_down + trig_gap*2)) # wait until the end of the 1 second of the light being on
+    else:
+        resp_time = 0
+
+    # before_second_light = time.time() - start_exp
+    pixels.fill(blank)
+    # after_second_light = time.time() - start_resp
+    if trig == 1: ## Maps out offset trigger to standard and target flashes
+        timestamp = local_clock()
+        outlet.push_sample([5], timestamp)
+    else:
+        timestamp = local_clock()
+        outlet.push_sample([6], timestamp)
+
+    return resp_time # before_second_light, after_second_light
+
+def get_resp(pin, wait_time, prev_delay, resp, trig): # get response (if not in the first second) + wait for wait time (delay)
+    start_resp = time.time()
+
+    GPIO.wait_for_edge(pin,GPIO.RISING, timeout = int(wait_time * 1000))
+
+    delay_end_time = time.time() - start_resp
+
+    if resp == 0:
+        resp_time = delay_end_time + prev_delay
+        if resp_time <= 2.0:
+            resp_trig(trig)
+    else:
+        resp_time = resp
+
+    if delay_end_time < wait_time:
+        time.sleep(wait_time - delay_end_time)
+
+    return resp_time
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos*3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos*3)
+        g = 0
+        b = int(pos*3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos*3)
+        b = int(255 - pos*3)
+    return (r, g, b)
+
+
+def rainbow_cycle(wait, rainbow_time):
+    start = time.time()
+    while time.time() - start < rainbow_time:
+        for j in range(255):
+            for i in range(pin_num):
+                pixel_index = (i * 256 // pin_num) + j
+                pixels[i] = wheel(pixel_index & 255)
+            pixels.show()
+            time.sleep(wait)
+
+def refresh_fixation(): 
+    img[int(width-width/100):int(width+width/100), int(height-height/20):int(height+height/20), :] = 255
+    img[int(width-width/20):int(width+width/20), int(height-height/100):int(height+height/100), :] = 255
+
+##distribution of targets and standards##
+trials = np.zeros(int(trial_num*standard_rate)).tolist() + np.ones(int(trial_num*target_rate)).tolist()
+shuffle(trials) # randomize order of standards and targets
+
+##variables to save trial information##
 trig_time   = []
 trig_type = []
 delay_length  = []
+trial_resp = []
+jitter_length = []
+resp_latency = []
+block_start_stop = []
+exp_start_stop = []
+trial_count = 0
 
-###set triggers to 0###
-GPIO.output(pi2trig('s',15),0)
-GPIO.output(pi2trig('r',15),0)
+##setup our neopixels##
+pixels = neopixel.NeoPixel(pin_out, pin_num, brightness = brightness, auto_write = True)
 
-###setup our instruction screens###
-pygame.font.init()
-myfont = pygame.font.SysFont('Times New Roman', 20)
-instructions1 = myfont.render('Focus on the middle two LEDs.', True, white)
-instructions2 = myfont.render('Press the spacebar when you see a blue flash, do NOT press the spacebar when you see a green flash.', True, white)
-instructions3 = myfont.render('Press the spacebar when you are ready to start.', True, white)
-break_screen = myfont.render('Feel free to take a break at this time. Press the spacebar when you are ready to start.', True, white)
-end_screen = myfont.render('Congratulations, you have finished the experiment! Please contact the experimenter.', True, white)
-
-###define led strip object, set brightness, set LEDs to zero in case they are on###
-blinkt.clear()
-blinkt.show()
-
-###show our instructions, and wait for a response###
-pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
-pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
-screen.blit(instructions1,(x_center-((instructions1.get_rect().width)/2),y_center+((instructions1.get_rect().height)*1)+10))
-screen.blit(instructions2,(x_center-((instructions2.get_rect().width)/2),y_center+((instructions2.get_rect().height)*2)+10))
-screen.blit(instructions3,(x_center-((instructions3.get_rect().width)/2),y_center+((instructions3.get_rect().height)*3)+10))
-pygame.display.flip()
-key_pressed = 0
-pygame.event.clear()
-while key_pressed == 0:
-    event = pygame.event.wait()
-    if event.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit()
-    elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-            key_pressed = 1
-
-screen.fill(pygame.Color("black")) 
-pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
-pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
-pygame.display.flip()
-time.sleep(1)
-
-###wait for button press to start experiment###
-vid_start = time.time()
-timestamp = local_clock()
-outlet.push_sample([3], timestamp)
-GPIO.output(pi2trig('s',3),1)
-time.sleep(1)
-GPIO.output(pi2trig('s',3),0)
-
-for i_tone in range(len(tones)):
-    if i_tone in (int(trials*0.10),int(trials*0.20),int(trials*0.30),int(trials*0.40),int(trials*0.50),int(trials*0.60),int(trials*0.70),int(trials*0.80),int(trials*0.90)):
-        ###show the break screen, and wait for a response###
+##to significy the start of the experiment we make the LEDs all red initially and then wait for a certain amount of time##
+for block in range(block_num):
+    pixels.fill(red)
+    GPIO.wait_for_edge(resp_pin,GPIO.RISING) ## Waits for an initial button press to turn on the LED (red)
+    timestamp = local_clock()
+    outlet.push_sample([3], timestamp)
+    GPIO.output(muse_pin,1) # send unique trigger for the start of the block
+    if block == 0:
+        start_exp = time.time()
+        exp_start_stop.append(0)
+    trig_time.append(time.time() - start_exp)
+    block_start_stop.append(time.time() - start_exp) # start of each block from start_exp
+    ## structure output of CSV
+    trig_type.append(3)
+    delay_length.append(2)
+    trial_resp.append(0)
+    jitter_length.append(0)
+    resp_latency.append(0)
+    time.sleep(2) ## leave red on for 2 seconds
+    pixels.fill(blank)
+    GPIO.output(muse_pin,0)
+    time.sleep(2)
+    for i_trial in range(len(trials)):
+        start_trial = time.time() + trig_gap # define start time of a given trial
+        delay = ((randint(0,500)*0.001)+1.0) # define delay, to be used later
+        delay_length.append(delay)
+        ##determine the type of stimuli we will show on this trial##
+        if trials[i_trial] == 0: #standards
+            trig = 1
+            pixels.fill(green)
+        elif trials[i_trial] == 1: #targets
+            trig = 2
+            pixels.fill(blue)
         timestamp = local_clock()
-        outlet.push_sample([4], timestamp)
-        GPIO.output(pi2trig('s',4),1)
-        time.sleep(1)
-        screen.fill(pygame.Color("black"))
-        pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
-        pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
-        screen.blit(break_screen,(x_center-((break_screen.get_rect().width)/2),y_center+10))
-        pygame.display.flip()
-        key_pressed = 0
-        pygame.event.clear()
-        while key_pressed == 0:
-            event = pygame.event.wait()
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    key_pressed = 1
-        screen.fill(pygame.Color("black")) 
-        pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
-        pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
-        pygame.display.flip()
-        time.sleep(1)
-        GPIO.output(pi2trig('s',4),0)
-    ###wait for a random amount of time between tones###
-    delay = ((randint(0,500))*0.001)+1.00
-    delay_length.append(delay)
-    if tones[i_tone] == 0:#standard
-        trig_type.append(1)
-        ### all LEDS###
-        ###set LEDs as green###
-        for i_pixel in range(8):
-            blinkt.set_pixel(i_pixel,0,25,0)
-        ###update LED colours and send trigger###
-        timestamp = local_clock()
-        outlet.push_sample([1], timestamp)
-        GPIO.output(pi2trig('s',1),1)
-        trig_time.append(time.time() - vid_start)
-    elif tones[i_tone] == 1:#targets
-        trig_type.append(2)
-        ### all LEDS###
-        ###set LEDs as blue###
-        for i_pixel in range(8):
-            blinkt.set_pixel(i_pixel,0,0,25)
-        ###update LED colours and send trigger###
-        timestamp = local_clock()
-        outlet.push_sample([2], timestamp)
-        GPIO.output(pi2trig('s',2),1)
-        trig_time.append(time.time() - vid_start)
-    blinkt.show()
-    ###wait for a bit###
-    time.sleep(1)
-    ###set triggers and colours back to zero###
-    blinkt.clear()
-    ###update LEDs###
-    blinkt.show()
-    ###wait for a random amount of time###
-    GPIO.output(pi2trig('s',15),0)
-    GPIO.output(pi2trig('r',15),0)
-    time.sleep(delay)
+        outlet.push_sample([3], timestamp)
+        GPIO.output(muse_pin,1)
+        trig_type.append(trig)
+        trig_time.append(time.time() - start_exp)
+        time.sleep(trig_gap)
 
-###show the end screen###
-timestamp = local_clock()
-outlet.push_sample([5], timestamp)
-GPIO.output(pi2trig('s',5),1)
-screen.fill(pygame.Color("black")) 
-screen.blit(end_screen,(x_center-((end_screen.get_rect().width)/2),y_center+10))
-pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
-pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
-pygame.display.flip()
-key_pressed = 0
-pygame.event.clear()
-while key_pressed == 0:
-        event = pygame.event.wait()
-        if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                        key_pressed = 1
-pygame.mouse.set_visible(0)
-GPIO.output(pi2trig('s',5),0)
+        resp_time = get_resp_led_off(resp_pin, 1.0,trig)
+        resp_time = get_resp(resp_pin, delay, 1.0, resp_time,trig)
+        resp_latency.append(time.time() - start_exp)
+        trial_resp.append(resp_time)
 
-###save times###
-filename_part = ("/home/pi/research_experiments/Experiments/" + exp_loc + "/Data/" + device + "/LSL_Trial_Information/" + partnum + "_" + filename + ".csv")
+        GPIO.output(muse_pin,0)
+        time.sleep(trig_gap)
+        end_trial = time.time()
 
-numpy.savetxt(filename_part, (trig_type,trig_time,delay_length), delimiter=',',fmt="%s")
+        actual_trial_length = end_trial - start_trial
+        theoretical_trial_length = delay + 1.0
+        jitter = actual_trial_length - theoretical_trial_length
+        jitter_length.append(jitter)
 
-pygame.display.quit()
-pygame.quit()
+        trial_count += 1
+    ##end of block##
+    pixels.fill(red)
+    trig_time.append(time.time() - start_exp)
+    block_start_stop.append(time.time() - start_exp) # end of each block from start_exp
+    ## structure output of CSV
+    trig_type.append(4)
+    delay_length.append(2)
+    trial_resp.append(0)
+    jitter_length.append(0)
+    resp_latency.append(0)
+    time.sleep(2) ## leave red on for 2 seconds
+    pixels.fill(blank)
+    time.sleep(2)
+
+# End of the experiment           
+exp_start_stop.append(time.time() - start_exp)
+
+rainbow_cycle(0.001, 5) ## After all blocks flash a rainbow at a refresh of (1st arguement) ms for (2nd arguement) seconds
+
+pixels.fill(blank)
+
+###save trial information###
+while os.path.isfile("/home/pi/research_experiments/Experiments/Visual_P3_LED/Data/Muse/LSL_Trial_Information/" + partnum + "_" + filename + ".csv") == True:
+    if int(partnum) >=10:
+        partnum = "0" + str(int(partnum) + 1)
+    else:
+        partnum = "00" + str(int(partnum) + 1)
+
+filename_part = ("/home/pi/research_experiments/Experiments/Visual_P3_LED/Data/Muse/LSL_Trial_Information/" + partnum + "_" + filename + ".csv")
+
+# What is each thing
+# trig_type
+# trig_time
+# delay_length
+# trial_resp
+# jitter_length
+# resp_latency
+# start_stop
+
+np.savetxt(filename_part, (trig_type,trig_time, delay_length, trial_resp, jitter_length, resp_latency, block_start_stop, exp_start_stop), delimiter=',',fmt="%s")
+
 GPIO.cleanup()
 
 if os.path.isfile("/home/pi/research_experiments/Stop_EEG2.csv") == True:  
--- Visual_P3_LED/Task/Visual_P3_LED_blinka.py
+++ Visual_P3_LED/Task/Visual_P3_LED_blinka.py
@@ -2,6 +2,7 @@ import time
 import os
 import sys
 from random import randint, shuffle
+import blinkt
 import datetime
 import numpy
 import pygame
@@ -13,10 +14,10 @@ device = sys.argv[2]
 filename = sys.argv[3]
 exp_loc = sys.argv[4]
 
-#create our stream variables###
+###create our stream variables###
 info = StreamInfo('Markers', 'Markers', 1, 0, 'int32', 'myuidw43536')
 
-#next make an outlet to record the streamed data###
+###next make an outlet to record the streamed data###
 outlet = StreamOutlet(info)
 
 ###initialise pygame###
@@ -25,8 +26,11 @@ pygame.init()
 pygame.display.init()
 
 ###setup pins for triggers###
-GPIO.setup([4,17,27,22,5,6,13,19,23,25],GPIO.OUT)
+GPIO.setup([4,17,27,22,5,6,13,19],GPIO.OUT)
 
+###Setup our function to send triggers###
+###trig_type is either 's' or 'r'###
+###trig_num must be between 1-15###
 def pi2trig(trig_type,trig_num):
     
     if trig_type == 's':
@@ -47,8 +51,6 @@ def pi2trig(trig_type,trig_num):
         if bin_num[i_trig] == '1':
             trig_pins.insert(trig_pos,pi_pins[i_trig])
             trig_pos = trig_pos + 1
-    trig_pins.insert(trig_pos+1,23)
-    trig_pins.insert(trig_pos+2,25)
     
     return trig_pins
 
@@ -60,10 +62,6 @@ x_center = disp_info.current_w/2
 y_center = disp_info.current_h/2
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255,255,255)
-rect_size = 100
-rect_green = (0,255,0)
-rect_blue = (0,0,255)
-rect_black = (0,0,0)
 
 ###FOR DEBUG###
 #screen = pygame.display.set_mode((200,100),pygame.RESIZABLE)
@@ -72,6 +70,8 @@ rect_black = (0,0,0)
 #####
 
 ##### Define variables #####
+partnum = "001"
+
 ###define the number of trials, and tones per trial###
 trials = 10
 low_rate = 0.8
@@ -91,18 +91,21 @@ delay_length  = []
 ###set triggers to 0###
 GPIO.output(pi2trig('s',15),0)
 GPIO.output(pi2trig('r',15),0)
-GPIO.output([23,25],0)
 
 ###setup our instruction screens###
 pygame.font.init()
 myfont = pygame.font.SysFont('Times New Roman', 20)
-instructions1 = myfont.render('Focus on the central fixation.', True, white)
-instructions2 = myfont.render('Press the spacebar for blue, do NOT press the spacebar for green.', True, white)
+instructions1 = myfont.render('Focus on the middle two LEDs.', True, white)
+instructions2 = myfont.render('Press the spacebar when you see a blue flash, do NOT press the spacebar when you see a green flash.', True, white)
 instructions3 = myfont.render('Press the spacebar when you are ready to start.', True, white)
 break_screen = myfont.render('Feel free to take a break at this time. Press the spacebar when you are ready to start.', True, white)
-end_screen = myfont.render('Congratulations, you have finished! Please contact the experimenter.', True, white)
+end_screen = myfont.render('Congratulations, you have finished the experiment! Please contact the experimenter.', True, white)
 
-#show our instructions, and wait for a response###
+###define led strip object, set brightness, set LEDs to zero in case they are on###
+blinkt.clear()
+blinkt.show()
+
+###show our instructions, and wait for a response###
 pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
 pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
 screen.blit(instructions1,(x_center-((instructions1.get_rect().width)/2),y_center+((instructions1.get_rect().height)*1)+10))
@@ -124,72 +127,85 @@ screen.fill(pygame.Color("black"))
 pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
 pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
 pygame.display.flip()
-pygame.time.wait(1000)
+time.sleep(1)
 
 ###wait for button press to start experiment###
+vid_start = time.time()
 timestamp = local_clock()
-vid_start = pygame.time.get_ticks()
 outlet.push_sample([3], timestamp)
-trig_time.append(pygame.time.get_ticks() - vid_start)
-trig_type.append(3)
 GPIO.output(pi2trig('s',3),1)
-GPIO.output([23,25],1)
-delay_length.append(0)
-pygame.time.wait(500)
-GPIO.output([23,25],0)
+time.sleep(1)
 GPIO.output(pi2trig('s',3),0)
-screen.fill(pygame.Color("black"))
-pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
-pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
-pygame.display.flip()
-pygame.time.wait(500)
 
 for i_tone in range(len(tones)):
+    if i_tone in (int(trials*0.10),int(trials*0.20),int(trials*0.30),int(trials*0.40),int(trials*0.50),int(trials*0.60),int(trials*0.70),int(trials*0.80),int(trials*0.90)):
+        ###show the break screen, and wait for a response###
+        timestamp = local_clock()
+        outlet.push_sample([4], timestamp)
+        GPIO.output(pi2trig('s',4),1)
+        time.sleep(1)
+        screen.fill(pygame.Color("black"))
+        pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
+        pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
+        screen.blit(break_screen,(x_center-((break_screen.get_rect().width)/2),y_center+10))
+        pygame.display.flip()
+        key_pressed = 0
+        pygame.event.clear()
+        while key_pressed == 0:
+            event = pygame.event.wait()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    key_pressed = 1
+        screen.fill(pygame.Color("black")) 
+        pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
+        pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_center+10),4)
+        pygame.display.flip()
+        time.sleep(1)
+        GPIO.output(pi2trig('s',4),0)
     ###wait for a random amount of time between tones###
-    delay = ((randint(0,500)))+1000
+    delay = ((randint(0,500))*0.001)+1.00
     delay_length.append(delay)
     if tones[i_tone] == 0:#standard
         trig_type.append(1)
+        ### all LEDS###
+        ###set LEDs as green###
+        for i_pixel in range(8):
+            blinkt.set_pixel(i_pixel,0,25,0)
         ###update LED colours and send trigger###
         timestamp = local_clock()
         outlet.push_sample([1], timestamp)
-        trig_time.append(pygame.time.get_ticks() - vid_start)
         GPIO.output(pi2trig('s',1),1)
-        GPIO.output([23,25],1)
-        rect1 = pygame.draw.rect(screen, rect_green , ((x_center + 20),(y_center-(rect_size/2)),rect_size,rect_size))
-        rect2 = pygame.draw.rect(screen, rect_green , ((x_center - (20+rect_size)),(y_center-(rect_size/2)),rect_size,rect_size))
-        pygame.display.update([rect1,rect2])
+        trig_time.append(time.time() - vid_start)
     elif tones[i_tone] == 1:#targets
         trig_type.append(2)
+        ### all LEDS###
+        ###set LEDs as blue###
+        for i_pixel in range(8):
+            blinkt.set_pixel(i_pixel,0,0,25)
         ###update LED colours and send trigger###
         timestamp = local_clock()
         outlet.push_sample([2], timestamp)
-        trig_time.append(pygame.time.get_ticks() - vid_start)
         GPIO.output(pi2trig('s',2),1)
-        GPIO.output([23,25],1)
-        rect1 = pygame.draw.rect(screen, rect_blue , ((x_center + 20),(y_center-(rect_size/2)),rect_size,rect_size))
-        rect2 = pygame.draw.rect(screen, rect_blue , ((x_center - (20+rect_size)),(y_center-(rect_size/2)),rect_size,rect_size))
-        pygame.display.update([rect1,rect2])
+        trig_time.append(time.time() - vid_start)
+    blinkt.show()
     ###wait for a bit###
-    pygame.time.wait(1000)
+    time.sleep(1)
     ###set triggers and colours back to zero###
+    blinkt.clear()
+    ###update LEDs###
+    blinkt.show()
     ###wait for a random amount of time###
-    rect1 = pygame.draw.rect(screen, rect_black , ((x_center + 20),(y_center-(rect_size/2)),rect_size,rect_size))
-    rect2 = pygame.draw.rect(screen, rect_black , ((x_center - (20+rect_size)),(y_center-(rect_size/2)),rect_size,rect_size))
-    pygame.display.update([rect1,rect2])
     GPIO.output(pi2trig('s',15),0)
     GPIO.output(pi2trig('r',15),0)
-    GPIO.output([23,25],0)
-    pygame.time.wait(delay)
+    time.sleep(delay)
 
 ###show the end screen###
 timestamp = local_clock()
 outlet.push_sample([5], timestamp)
-trig_time.append(pygame.time.get_ticks() - vid_start)
-trig_type.append(5)
 GPIO.output(pi2trig('s',5),1)
-GPIO.output([23,25],1)
-delay_length.append(0)
 screen.fill(pygame.Color("black")) 
 screen.blit(end_screen,(x_center-((end_screen.get_rect().width)/2),y_center+10))
 pygame.draw.line(screen, (255, 255, 255), (x_center-10, y_center), (x_center+10, y_center),4)
@@ -197,28 +213,27 @@ pygame.draw.line(screen, (255, 255, 255), (x_center, y_center-10), (x_center, y_
 pygame.display.flip()
 key_pressed = 0
 pygame.event.clear()
-pygame.time.wait(500)
 while key_pressed == 0:
-    event = pygame.event.wait()
-    if event.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit()
-    elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-            key_pressed = 1
-GPIO.output([23,25],0)
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                        key_pressed = 1
 pygame.mouse.set_visible(0)
+GPIO.output(pi2trig('s',5),0)
 
 ###save times###
 filename_part = ("/home/pi/research_experiments/Experiments/" + exp_loc + "/Data/" + device + "/LSL_Trial_Information/" + partnum + "_" + filename + ".csv")
 
-numpy.savetxt(filename_part, (trig_type,trig_time, delay_length), delimiter=',',fmt="%s")
+numpy.savetxt(filename_part, (trig_type,trig_time,delay_length), delimiter=',',fmt="%s")
 
 pygame.display.quit()
 pygame.quit()
 GPIO.cleanup()
 
-if os.path.isfile("/home/pi/research_experiments/Stop_EEG2.csv") == True:   
+if os.path.isfile("/home/pi/research_experiments/Stop_EEG2.csv") == True:  
     time.sleep(5)
     os.remove("/home/pi/research_experiments/Stop_EEG2.csv")
     time.sleep(5)
