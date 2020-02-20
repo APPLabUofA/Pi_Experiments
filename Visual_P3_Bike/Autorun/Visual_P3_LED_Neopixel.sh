1380c76150f8f1e5bc5dc19a3d4b61a28e7ede0b
kuziekj <kuziek@ualberta.ca>
2020-02-20 09:13:25 -0700

    Add files via upload

---
+++ Visual_P3_Bike/Autorun/Visual_P3_LED_Neopixel.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/pi/.virtualenvs/py3cv4/bin/activate
+sudo python3 /home/pi/research_experiments/Experiments/Visual_P3_LED_Neopixel/Autorun/run_exp.py
---
+++ Visual_P3_Bike/Autorun/run_exp.py
@@ -0,0 +1,9 @@
+import os
+import time
+
+time.sleep(5)
+
+os.system('sudo python3 /home/pi/research_experiments/Experiments/Visual_P3_LED_Neopixel/Task/visual_p3_led_neopixel_bike.py')
+
+time.sleep(5)
+os.system("sudo shutdown -h now")
